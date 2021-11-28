4a8be5df95ababfd4087245847abea214624455d
Josip-github <josipcota1@gmail.com>
2021-11-28 16:27:40 +0100

    This commit develops controller class The class is annotated with @RestController These 4 methods handle requests for retrieving and by doing that they rely on service layer which was instantiated before everything and annotated with @Autowired on setter method.

---
+++ src/main/java/com/udacity/dogs/controller/DogController.java
@@ -0,0 +1,47 @@
+package com.udacity.dogs.controller;
+
+import com.udacity.dogs.model.Dog;
+import com.udacity.dogs.service.DogService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DogController {
+
+    private DogService dogService;
+
+    @Autowired
+    public void setDogService(DogService dogService) {
+        this.dogService = dogService;
+    }
+
+    @GetMapping("/dogs")
+    public ResponseEntity<List<Dog>> getAllDogs(){
+        List<Dog> dogs = dogService.retrieveDogs();
+        return new ResponseEntity<List<Dog>>(dogs, HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/breeds")
+    public ResponseEntity<List<String>> getDogBreeds(){
+        List<String> breeds = dogService.retrieveDogBreed();
+        return new ResponseEntity<List<String>>(breeds, HttpStatus.OK);
+    }
+
+    @GetMapping("/breed/{id}")
+    public ResponseEntity<String> getBreedById(@PathVariable Long id){
+        String dogBreed = dogService.retrieveDogBreedById(id);
+        return new ResponseEntity<String>(dogBreed, HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/names")
+    public ResponseEntity<List<String>> getDogNames(){
+        List<String> names = dogService.retrieveDogNames();
+        return new ResponseEntity<List<String>>(names, HttpStatus.OK);
+    }
+}
