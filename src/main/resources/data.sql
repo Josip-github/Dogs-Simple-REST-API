6c761a6dd2f07fc140058e9d95c4c06ef9d16b75
Josip-github <josipcota1@gmail.com>
2021-11-28 16:41:58 +0100

    This commit populates database with some data

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO dog (id, name, breed, origin) VALUES (1, 'zlatko', 'retriver', 'europa');
+INSERT INTO dog (id, name, breed, origin) VALUES (2, 'ivek', 'rex', 'hrv');
+INSERT INTO dog (id, name, breed, origin) VALUES (3, 'bonni', 'buldog', 'njem');
+INSERT INTO dog (id, name, breed, origin) VALUES (4, 'ares', 'ovcar', 'posavina');
+INSERT INTO dog (id, name, breed, origin) VALUES (5, 'ruzzi', 'chiuwahuwa', 'bIh');
