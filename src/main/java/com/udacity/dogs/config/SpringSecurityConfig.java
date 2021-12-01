8e0fa5726427b9d68a287d219d2067fd0488df30
Josip-github <josipcota1@gmail.com>
2021-12-01 17:05:39 +0100

    This commit creates configureGlobal() method Line 29 creates an in-memory user Line 30 sets the username as "admin" Line 31 sets the password as "password" and by that relies on encoder() method from the line 36 Line 32 gives a user a simple role as a "USER"

--- src/main/java/com/udacity/dogs/config/SpringSecurityConfig.java
+++ src/main/java/com/udacity/dogs/config/SpringSecurityConfig.java
@@ -1,9 +1,14 @@
 package com.udacity.dogs.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -19,5 +24,16 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and().httpBasic();
     }
 
+    @Autowired
+    protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder().encode("password"))
+                .roles("USER");
+    }
 
+    @Bean
+    public PasswordEncoder encoder(){
+        return new BCryptPasswordEncoder();
+    }
 }
